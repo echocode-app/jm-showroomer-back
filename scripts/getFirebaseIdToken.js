73c57f9ea212920969deb608501ebc412e1f581c
AnnaK <kotlyaranya1771@gmail.com>
2026-01-30 08:17:20 +0200

    id token

---
+++ scripts/getFirebaseIdToken.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKG_oI5avZzeM56ZN7euSU7tP4jVLMCX0",
+  authDomain: "jm-showroom.firebaseapp.com",
+  projectId: "jm-showroom",
+  appId: "1:725198091196:web:500025b57d7fdc2e29f1b9",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+async function login() {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+
+  const user = result.user;
+  const idToken = await user.getIdToken(true);
+
+  console.log("\n🔥 FIREBASE ID TOKEN 🔥\n");
+  console.log(idToken);
+  console.log("\nUID:", user.uid);
+  console.log("EMAIL:", user.email);
+}
+
+login().catch(console.error);
+
+
+// curl https://jm-showroomer-back.onrender.com/api/v1/users/me \
+//   -H "Authorization: Bearer <TOKEN>"
